5b750e215e71b57b308eb9b90e70c4ecbca6fe8f
cesarrecalde <ce.sarrecalde@hotmail.com>
2015-03-18 09:22:55 -0300

    Merge branch 'master' of /home/cesar/pycharm_proyects/skp with conflicts.

--- apps/autenticacion/forms.py
+++ apps/autenticacion/forms.py
@@ -19,13 +19,13 @@ class FormularioRegistro(UserCreationForm):
     email = forms.EmailField(required=True)
 	
 class Meta:
-		model = User
-		fields = ('username', 'email', 'password1', 'password2')
+	model = User
+	fields = ('username', 'email', 'password1', 'password2')
 	def save(self, commit=True):
 		user = super(FormularioRegistro, self).save(commit=False)
 		user.email = self.cleaned_data['email']
 
 		if commit:
 			user.save()
-
+	
 		return user
