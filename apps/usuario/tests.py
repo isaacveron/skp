9a11cd17d31a412578d97e3d5ad2e74d709833e1
isaac <isaacveron@gmail.com>
2015-04-10 17:44:38 -0400

    documentacion

--- apps/usuario/tests.py
+++ apps/usuario/tests.py
@@ -1,38 +1,24 @@
+from django.db.models import 
+from django.test import Client
 from django.test import TestCase
-import unittest, time
-from datetime import datetime
-from django.contrib.auth.models import User, Group, Permission
-from django.core.urlresolvers import reverse
-# Create your tests here.
+from django.contrib.auth.models import User, Group
 
-class TestUserBD(TestCase):
-   def test_numero_elementos(self):
-       print("\nTEST: Al crear la BD el numero de usuarios debe ser 0")
-       try:
-           self.assertEqual(0,len(User.objects.all()))
-       except:
-           print("Prueba fallida, el numero de usuarios es distinto de 0")
-           return
-       print("Prueba exitosa, el numero de usuarios es igual a 0")
+class TestCase(TestCase):
 
-class TestLogin(TestCase):
-   usuario='admin'
-   password='admin'
-   #cargamos los usuarios
-   fixtures = ["usuario"]
-   def test_login_usuario(self):
-       print("\nTEST: Loguear usuario registrado")
-       try:
-           # vamos a la pantalla de inicio
-           resp = self.client.get('/')
-           self.assertEqual(resp.status_code, 200)
-           # logueamos con el usuario sgp
-           login = self.client.login(username=self.usuario, password=self.password)
-           self.assertTrue(login)
-       except:
-           if resp.status_code == 404:
-               print("Prueba fallida, la url no existe")
-           else:
-               print("Prueba fallida, el usuario no existe o esta inactivo")
-           return
-       print("Prueba exitosa, el usuario pudo iniciar sesion")
+    fixtures = ["usuarios"]
+
+    username = 'admin'
+    password = 'admin'
+
+    def test_loguin_admin(self):
+        """ Test para loguear al administrador. """
+        
+        resp = self.client.get('/login/')                                           #Solicitud de la pagina de autenticacion
+        self.assertEqual(resp.status_code, 200)                                     #Pagina de login recibida con exito
+        login = self.client.login(username=self.username, password=self.password)   #Proceso de autenticacion
+        logout= self.client.logout()                                                #Cerramos la sesion actual
+        self.assertFalse(logout)                                                    #Probamos que efectivamente la sesion esta cerrada
+        resp = self.client.get('/index/')                                           #Pasamos a la pagina de inicio
+        self.assertNotEqual(resp.status_code, 200)                                  #Probamos que ya no podemos acceder al sistema si no estamos logueados
+        print 'Test de login administrador ejecutado exitosamente.'
+    
